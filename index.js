015c43b35abef67bb797f5e0e12d6ffc9e73f967
Visalinthan <visalinthan@gmail.com>
2023-12-08 17:00:02 +0100

    request map

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const WebSocket = require('wss');
+const WebSocket = require('ws');
 const axios = require('axios');
 const express = require('express');
 const app = express();
@@ -14,52 +14,59 @@ wss.on('connection', async (ws) => {
   console.log('WebSocket connecté');
 
   const baseURL = "https://newsapi.org/v2/"
-  const key = '7a76bca50c1d4dd1bfae499697eea7b4';
+  const key = '3ae6c7ef47764a919093ed68658ba917';
   let prefix = ''
 
   // Handle incoming messages
 
   ws.on('message', function incoming(request) {
-    console.log("keyword : " + request)
-    if (request == "all" || request == undefined) {
-      prefix = "everything?q=keyword"
-    } else if (request == "sources") {
-      prefix = "top-headlines/sources?"
-    } else {
-      prefix = `top-headlines?sources=${request}`
-    }
+    const jsonData = JSON.parse(request)
+    jsonData.forEach(element => {
+      console.log("keyword : " + element.key)
+      if (element.value == "all") {
+        prefix = "everything?q=keyword"
+      } else if (element.key == "list_sources") {
+        prefix = "top-headlines/sources?"
+      } else if (element.key == "one_source") {
+        prefix = `top-headlines?sources=${element.value}`
+      } else if (element.key == "category") {
+        prefix = `top-headlines?category=${element.value}`
+      }
+
+    });
+
 
     // executable
-    const executeTask = async () => {
-      try {
-        // @ts-ignore
-        const url = `${baseURL}${prefix}&apiKey=${key}`
-        const apiResponse = await axios.get(url);
-        const data = apiResponse.data;
-        console.log(url)
+     const executeTask = async () => {
+       try {
+         // @ts-ignore
+         const url = `${baseURL}${prefix}&apiKey=${key}`
+         const apiResponse = await axios.get(url);
+         const data = apiResponse.data;
+         
+ 
+         // passer au front
+         ws.send(JSON.stringify(data));
+       } catch (error) {
+         console.error('Error fetching data from API:', error.message);
+       }
+     };
 
-        // passer au front
-        ws.send(JSON.stringify(data));
-      } catch (error) {
-        console.error('Error fetching data from API:', error.message);
-      }
-    };
-
-    // exécuter immédiat
-    executeTask();
-
-    // tous les 10mins
-    const intervalId = setInterval(executeTask, 10 * 60 * 1000);
-
-    // 当WebSocket连接关闭时
-    ws.on('close', () => {
-      console.log('WebSocket close');
-
-      // effacer l'intervalle
-      clearInterval(intervalId);
-    });
+ 
+     // exécuter immédiat
+     executeTask();
+ 
+     // tous les 10mins
+     const intervalId = setInterval(executeTask, 10 * 60 * 1000);
+ 
+     // 当WebSocket连接关闭时
+     ws.on('close', () => {
+       console.log('WebSocket close');
+ 
+       // effacer l'intervalle
+       clearInterval(intervalId);
+     });
   });
-
 
 
 });
